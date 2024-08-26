9d6306aaf76b1d121c0a2d2e7e6f48d374a2580c
alwinsDen <jsmadesimple@gmail.com>
2024-08-26 11:21:43 +0530

    first

---
+++ js/app.js
@@ -0,0 +1,33 @@
+window.addEventListener("load",()=> {
+  const mobileRef = document.getElementById("app_render");
+  const clickRef = document.getElementById("app_clicker");
+  let startTime = null;
+  let startPos = null;
+  function eventBalancer(stringName,event) {
+    const bbdBox = mobileRef.getBoundingClientRect();
+    const scaleX  = 1179 / mobileRef.clientWidth;
+    const scaleY = 2556 / mobileRef.clientHeight;
+    let x = (event.clientX - bbdBox.left)*scaleX;
+    let y = (event.clientY - bbdBox.top)*scaleY;
+    return {x, y}
+  }
+  mobileRef.addEventListener("mousedown", (event)=> {
+    startTime = Date.now();
+    startPos = eventBalancer("mousedown", event);
+  })
+
+  mobileRef.addEventListener("mouseup", (event)=> {
+    let endTime = Date.now();
+    if ((endTime - startTime) < 200) {
+      const pressEvent = eventBalancer("click", event);
+      console.log(`idb ui click ${Math.round(pressEvent.x)} ${Math.round(pressEvent.y)}`
+      )
+      clickRef.style.left = `${event.clientX}px`;
+      clickRef.style.top = `${event.clientY}px`;
+    } else {
+      let endPos = eventBalancer("mouseup", event);
+      console.log(`idb ui swipe ${Math.round(startPos.x)} ${Math.round(startPos.y)} ${Math.round(endPos.x)} ${Math.round(endPos.y)} --duration ${(endTime - startTime)/1000}`)
+    }
+  })
+})
+
